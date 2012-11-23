f16bcb64ab37985e3e5023cab731de2edddae755
benjilanyado <benjilanyado@gmail.com>
2012-11-23 16:22:29 +0000

    changed migrations

--- db/migrate/20121122174759_change_range_to_integer.rb
+++
@@ -1,9 +0,0 @@
-class ChangeRangeToInteger < ActiveRecord::Migration
- def up
-  	change_column :sections, :range, :integer
-  end
-
-  def down
-  	change_column :sections, :range, :string
-  end
-end
