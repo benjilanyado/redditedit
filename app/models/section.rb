d05c8aa9291f976771d0991ebf2afdb958768bf4
benjilanyado <benjilanyado@gmail.com>
2012-12-04 10:48:16 +0000

    new get content function

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,27 +4,32 @@ class Section < ActiveRecord::Base
   has_many :stories
 
   def get_content
-    stories.each { |s| s.delete }
+    to_delete = stories.all
 
     request = HTTParty.get(url)
 
     doc = Nokogiri::HTML(request.body)
     r = eval(range)
-
+    stories_added = 0
+    
     doc.css(anchor)[r].each_with_index do |entry, index|
 
-	  	story = self.stories.new
-	  	story.title = entry.text
-	  	story.url = entry[:href]
-	  	
+      story = self.stories.new
+      story.title = entry.text
+      story.url = entry[:href]
+          
       if (id.to_i < 8)
         story.source = doc.css('#siteTable .domain a')[r.first + index].text
       end #end of if statement
 
-      story.save
-
-   	end #end of first entry do loop
-
-  end #end of function
+      if story.save
+        stories_added += 1
+      end
+    end
+    
+    if stories_added > 0
+      to_delete.each { |s| s.delete }
+    end
+end
  
 end #end of class
