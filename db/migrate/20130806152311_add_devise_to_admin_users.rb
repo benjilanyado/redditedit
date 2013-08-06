207aa4ceaf81615d0bb3d06fc9669f357b76b769
Benji Lanyado <benjilanyado@gmail.com>
2013-08-06 17:26:23 +0100

    sections in admin

--- db/migrate/20130806152311_add_devise_to_admin_users.rb
+++ db/migrate/20130806152311_add_devise_to_admin_users.rb
@@ -1,10 +1,6 @@
 class AddDeviseToAdminUsers < ActiveRecord::Migration
 
-  def migrate(direction)
-    super
-    # Create a default user
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') if direction == :up
-  end
+
 
   def self.up
     
