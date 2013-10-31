b1b2527487361ee62a50a2975708704c4bc3a919
Benji Lanyado <benjilanyado@gmail.com>
2013-10-31 19:59:08 +0000

    new line

--- config/application.rb
+++ config/application.rb
@@ -4,12 +4,12 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+   Bundler.require(:default, :assets, Rails.env)
 end
 
-config.assets.initialize_on_precompile = false
+
 
 module Rededit
   class Application < Rails::Application
